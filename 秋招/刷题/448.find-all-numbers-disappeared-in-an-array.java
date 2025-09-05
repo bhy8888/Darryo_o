a5751ebb75d15c7dc699058e56aa4abed76592e7
bhy <bhy8881@gmail.com>
2025-09-05 19:35:37 +0800

    250905提交

---
+++ 秋招/刷题/448.find-all-numbers-disappeared-in-an-array.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=448 lang=java
+ * @lcpr version=30204
+ *
+ * [448] 找到所有数组中消失的数字
+ */
+
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        int len = nums.length;
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [4,3,2,7,8,2,3,1]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,1]\n
+// @lcpr case=end
+
+ */
+
